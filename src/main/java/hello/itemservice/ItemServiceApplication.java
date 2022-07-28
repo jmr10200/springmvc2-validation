3c9b272b1c84354639d54d4f7197c7e7bae5b54c
jmr <jmr10200@gmail.com>
2022-07-28 20:47:14 +0900

    Ver1 검증 직접 처리

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,4 +10,19 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	/* 상품 저장 성공	*/
+	// 0. GET /add : 상품등록 폼을 표시
+	// 1. POST /add : 상품저장 실행
+	// 2. Redirect /items/{id} : 등록된 정보 전달
+	// 3. GET /items/{id} : 상품 상세 페이지 표시
+	// 사용자가 상품 등록 폼에서 정상 범위의 데이터를 입력하면, 서버에서는 검증 로직이 통과하고, 저장하여 상세화면으로 redirect 한다.
+
+	/* 상품 저장 검증 실패 */
+	// 0. GET /add : 상품 등록 폼 표시
+	// 1. POST /add : 상품 저장 실행
+	// 상품 저장 실패 : Model 에 검증 오류 결과 포함하여 반환
+	// 싱품 등록폼 표시
+	// 사용자가 상품 등록 폼에서 상품명을 입력하지 않거나, 가격, 수량 등이 너무 작거나 커서 검증 범위를 넘어서면,
+	// 서버 검증 로직이 실패해야한다. 검증에 실패한 경우 고객에게 다시 상품등록 폼을 보여주고,
+	// 어떤 값을 잘못 입력했는지 알려줘야 한다.
 }
--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -5,10 +5,13 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/validation/v1/items")
@@ -38,7 +41,42 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+        // validation check
+        // 검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            // 어떤 필드에서 발생했는지 구분하기위해 key 는 필드명으로 함
+            errors.put("itemName", "상품명은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증 (상관관계)
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                // 특정 필드명을 사용할수 없으므로 globalError 이라는 key를 지정
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+            }
+        }
+
+        // validation check : 검증 실패시 다시 입력 폼 표시
+        if (!errors.isEmpty()) {
+            // 검증 실패시 오류 메시지 출력하기위해 model 에 error 을 담아 입력 폼이 있는 뷰 템플릿으로 던진다
+            model.addAttribute("errors", errors);
+            // 입력 폼 표시
+            return "validation/v1/addForm";
+        }
+
+        // 검증 통과했을때 실행되는 성공로직
+        // 상품등록
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
